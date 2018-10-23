30ea3ba7a3f0acbcec03dea95ab2a2f0c1fab4f5
Matt Cadorette <mcadorette@gmail.com>
2018-10-23 13:39:43 -0400

    Initial commit from Atomist

---
+++ src/main/java/hhh001/Hhh001Application.java
@@ -0,0 +1,12 @@
+package hhh001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhh001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hhh001Application.class, args);
+	}
+}
